33a488f94921f81ef3a6a5d1fe5014adeef7e53e
se99388 <45286937+se99388@users.noreply.github.com>
2019-09-03 21:09:05 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//testing
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
